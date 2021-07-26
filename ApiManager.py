6da7348f80b6b772b77f1c425573020220399be9
Gavin1937 <gyh1837771475gyh@gmail.com>
2021-07-26 04:23:39 -0700

    add feature: auto refresh pixiv apitoken

--- ApiManager.py
+++ ApiManager.py
@@ -68,6 +68,7 @@ import json
 # api libs
 from pixivpy3 import *
 import tweepy
+from pixiv_auth import refresh_returnDict
 
 
 @Singleton
@@ -77,9 +78,11 @@ class PixivAPI:
     # private members
     __api: AppPixivAPI = None
     __auto_refresh_flag: bool = False
+    __apitoken_life: int = None
+    __last_update_life: int = None
     
     # constructor
-    def __init__(self):
+    def __init__(self, enable_autoRefreshToken: bool = True):
         apitoken_dict = {}
         # check whether apitoken.json exist
         if not os.path.isfile("./apitoken.json"): # file does not exist, create a new one
@@ -114,17 +117,68 @@ class PixivAPI:
             self.__api = None
             raise err
         
-        self.__autoRefreshing_token()
+        # get current apitoken life time
+        self.__apitoken_life = apitoken_dict["pixiv_token"]["expires_in"]
+        self.__last_update_life = self.__apitoken_life
+        
+        # setting __auto_refresh_flag
+        self.setAutoRefreshToken(enable_autoRefreshToken)
+    
+    def refreshApiToken(self):
+        # load apitoken from file
+        file = open("./apitoken.json", 'r')
+        apitoken_dict = json.load(file)
+        file.close()
+        # refresh token
+        res = refresh_returnDict(apitoken_dict["pixiv_token"]["refresh_token"])
+        # update token
+        apitoken_dict["pixiv_token"]["access_token"] = res["access_token"]
+        apitoken_dict["pixiv_token"]["refresh_token"] = res["refresh_token"]
+        apitoken_dict["pixiv_token"]["expires_in"] = res["expires_in"]
+        # write to apitoken.json
+        file = open("./apitoken.json", 'w')
+        json.dump(apitoken_dict, file)
+        file.close()
+        # backup apitoken.json
+        file = open("./apitoken.json.bak", 'w')
+        json.dump(apitoken_dict, file)
+        file.close()
+    
+    def updateApiTokenLifetime(self, new_lifetime: int):
+        if self.__auto_refresh_flag and new_lifetime <= 100: # refresh apitoken as needed
+            self.refreshApiToken()
+            self.__last_update_life = self.__apitoken_life
+        else: # healthy token, update lifetime
+            # modify lifetime
+            self.__apitoken_life = new_lifetime
+            if abs(self.__apitoken_life - self.__last_update_life) > 30: # minimize file writing
+                # load apitoken from file
+                file = open("./apitoken.json", 'r')
+                apitoken_dict = json.load(file)
+                file.close()
+                # update token
+                apitoken_dict["pixiv_token"]["expires_in"] = self.__apitoken_life
+                # write to file
+                file = open("./apitoken.json", 'w')
+                json.dump(apitoken_dict, file)
+                file.close()
+                # update last_update_life
+                self.__last_update_life = self.__apitoken_life
     
     def setAutoRefreshToken(self, flag):
         """Whether Enable or Disable auto refreshing pixiv api token"""
         self.__auto_refresh_flag = flag
     
+    def getApiTokenLifetime(self) -> int:
+        return self.__apitoken_life
+    
     # api features
     def getIllustDetail(self, pid: int) -> dict:
+        self.updateApiTokenLifetime(self.__apitoken_life-1)
         return self.__api.illust_detail(pid)
     
     def getUserDetail(self, pid: int) -> dict:
+        self.updateApiTokenLifetime(self.__apitoken_life-1)
         return self.__api.user_detail(pid)
     
     def getUserIllustList(self, pid: int, count: int) -> dict:
@@ -144,6 +198,8 @@ class PixivAPI:
             # move to next page
             next = self.__api.parse_qs(cur_page["next_url"])
             cur_page = self.__api.user_illusts(user_id=pid, offset=next["offset"])
+        # update apitoken lifetime
+        self.updateApiTokenLifetime(self.__apitoken_life-len(output))
         return output
     
     def searchIllust(
@@ -152,11 +208,14 @@ class PixivAPI:
             sort='date_desc', duration=None,
             start_date=None, end_date=None,
             filter='for_ios', offset=None):
-        return self.__api.search_illust(
+        output = self.__api.search_illust(
                 word, search_target=search_target,
                 sort=sort, duration=duration,
                 start_date=start_date, end_date=end_date,
                 filter=filter, offset=offset)
+        # update apitoken lifetime
+        self.updateApiTokenLifetime(self.__apitoken_life-len(output))
+        return output
     
     def searchUser(
             self, word,
@@ -164,18 +223,24 @@ class PixivAPI:
             duration=None,
             filter='for_ios',
             offset=None):
-        return self.__api.search_user(
+        output = self.__api.search_user(
                 word, sort=sort,
                 duration=duration,
                 filter=filter, offset=offset)
+        # update apitoken lifetime
+        self.updateApiTokenLifetime(self.__apitoken_life-len(output))
+        return output
     
     def downloadIllust(self, url: str, path: str=os.path.curdir, name: str=None) -> bool:
+        self.updateApiTokenLifetime(self.__apitoken_life-1)
         return self.__api.download(url=url, path=path, name=name)
     
     def followUser(self, pid: int):
+        self.updateApiTokenLifetime(self.__apitoken_life-1)
         self.__api.user_follow_add(user_id=pid)
     
     def unfollowUser(self, pid: int):
+        self.updateApiTokenLifetime(self.__apitoken_life-1)
         self.__api.user_follow_delete(user_id=pid)
     
     def api(self) -> AppPixivAPI:
@@ -190,8 +255,6 @@ class PixivAPI:
             apitoken_dict["pixiv_token"]["expires_in"] > 0 
         )
     
-    def __autoRefreshing_token(self):
-        pass
 
 
 
@@ -201,7 +264,6 @@ class TwitterAPI:
     
     # private members
     __api: tweepy.API = None
-    __auto_refresh_flag: bool = False
     
     # constructor
     def __init__(self):
@@ -245,10 +307,6 @@ class TwitterAPI:
         except Exception as err:
             self.__api = None
             raise err
-    
-    def setAutoRefreshToken(self, flag):
-        """Whether Enable or Disable auto refreshing pixiv api token"""
-        self.__auto_refresh_flag = flag
     
     # api features
     def getStatusJson(self, status_id: str) -> dict:
