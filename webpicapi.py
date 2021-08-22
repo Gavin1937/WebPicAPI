c0b6dda64cf1d9a14089233ccfdc0458224d90b9
Gavin1937 <gyh1837771475gyh@gmail.com>
2021-08-22 03:16:10 -0700

    clearfy comment in webpicapi.py

--- webpicapi.py
+++ webpicapi.py
@@ -3,7 +3,7 @@
 # ##################################################################
 # 
 # A simple API to fetch basic info and download pictures from
-# popular picture/wallpaper websites.
+# popular picture/wallpaper websites and social media.
 # 
 # Author: Gavin1937
 # GitHub: https://github.com/Gavin1937/WebPicAPI
