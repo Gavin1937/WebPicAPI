07e91916f92cb3b94cb1a88d937ce042c5623290
Gavin1937 <gyh1837771475gyh@gmail.com>
2022-05-22 01:26:36 -0700

    update Util __init__

--- WebPicAPI/Util/__init__.py
+++ WebPicAPI/Util/__init__.py
@@ -1,2 +1,6 @@
 
-from .httpUtilities import *
+from .httpUtilities import (pathCvt, randDelay, getSrc,
+                            getSrcStr, getSrcJson,
+                            writeStr2File, writeBytes2File,
+                            downloadFile)
+
