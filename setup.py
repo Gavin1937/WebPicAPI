ff7b1d12c6ab940192b7e613065660717558a1e6
Gavin1937 <gyh1837771475gyh@gmail.com>
2022-05-24 06:56:30 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ with open("./VERSION", "r") as file:
 with open("./README.md", "r") as file:
     readme = file.read()
 
+with open("./LICENSE", "r") as file:
+    license = file.read()
+
 setup(
     name="WebPicAPI",
     version=VERSION,
@@ -17,6 +20,9 @@ setup(
     author="Gavin1937",
     author_email="gyh1837771475gyh@gmail.com",
     url="https://github.com/Gavin1937/WebPicAPI",
+    license="GPL-3.0",
+    license_files=license,
+    install_requires=["PixivPy", "tweepy", "requests", "requests-oauthlib", "beautifulsoup4", "lxml"],
     packages=["WebPicAPI", "WebPicAPI.Api", "WebPicAPI.ApiManager", "WebPicAPI.Util"]
 )
 
